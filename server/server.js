2f030966d0694a7810d559aa5b096634c4fb23b3
piyush negi <piyushnegi959@gmail.com>
2026-09-05 22:05:22 +0530

    Allow Vercel frontend origin

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const app= express();
 
 const allowedOrigins = [
     'http://localhost:5173',
+    'https://project-00000.vercel.app',
     process.env.FRONTEND_URL,
 ].filter(Boolean);
 app.use(express.json());
