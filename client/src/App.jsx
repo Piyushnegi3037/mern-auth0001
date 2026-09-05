d6aac1c6066c9ee35386ccb9caad715b87a3a65f
piyush negi <piyushnegi959@gmail.com>
2026-09-05 20:31:42 +0530

    fix login import casting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home.jsx'
-import Login from './pages/login.jsx'
+import Login from './pages/Login.jsx'
 import EmailVerify from './pages/EmailVerify.jsx'
 import ResetPassword from './pages/ResetPassword.jsx'
 import {ToastContainer}from 'react-toastify'
