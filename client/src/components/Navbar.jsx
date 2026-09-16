d800e7ff59d9993b7de44ac1bc49f9baa80d2640
piyush negi <piyushnegi959@gmail.com>
2026-09-16 21:13:56 +0530

    final1.0

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,9 +4,11 @@ import { useNavigate } from 'react-router-dom'
 import { AppContent } from '../context/AppContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import { useState } from 'react'
 
 const Navbar = () => {
   const navigate = useNavigate()
+  const [menuOpen, setMenuOpen] = useState(false);
   const { userData, backendUrl, setUserData, setIsLoggedin } = useContext(AppContent)
   const sendVerificationOtp = async () => {
     try {
@@ -39,7 +41,7 @@ const Navbar = () => {
    }
 
   return (
-    <div className='absolute top-0 flex w-full items-center justify-between p-4 sm:p-6 sm:px-24'>
+    (<div className='absolute top-0 flex w-full items-center justify-between p-4 sm:p-6 sm:px-24'>
       <img src={logo} alt='' className='w-28 sm:w-32' />
 
       {userData ? (
@@ -60,7 +62,36 @@ const Navbar = () => {
         </button>
       )}
     </div>
-  )
-}
+// when i open it my mobile hover functonalyy not working i acant sse my logout or verfify email button
+ 
+      ))}
+
+//     <div className='absolute top-0 flex w-full items-center justify-between p-4 sm:p-6 sm:px-24'>
+//       <img src={logo} alt='' className='w-28 sm:w-32' />
+
+// {userData?(
+// <div 
+//   className="relative flex h-8 w-8 items-center justify-center rounded-full bg-black text-white"
+//   onClick={() => setMenuOpen(!menuOpen)}
+// >
+//   {userData.name[0].toUpperCase()}
+//   {menuOpen && (
+//     <div className="absolute right-0 top-0 z-10 w-40 rounded bg-gray-100 pt-10 text-black">
+//       <ul className="list-none p-2 text-sm">
+//         {!userData.isAccountVerified && (
+//           <li onClick={sendVerificationOtp} className="cursor-pointer bg-gray-200 px-2 py-1">verify email</li>
+//         )}
+//         <li onClick={logout} className="cursor-pointer bg-gray-200 px-2 py-1">logout</li>
+//       </ul>
+//     </div>
+//   )}
+// </div>):(
+//         <button onClick={() => navigate('/login')} type='button' className='rounded-full border border-gray-500 px-6 py-2 text-gray-800 transition-all hover:bg-gray-100'>
+//           Login
+//         </button>
+//       )}
+      
+//     </div>
+
 
 export default Navbar
