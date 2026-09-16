c790163fed30082d623267ad39d4f1ad3d686715
piyush negi <piyushnegi959@gmail.com>
2026-09-16 16:01:49 +0530

    fix productionissue

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -57,8 +57,8 @@ export const login =async(req,res)=>{
         const token= jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"});
         res.cookie('token',token, {
             httpOnly:true,
-            secure:true,
-            sameSite:'none',
+            secure:process.env.NODE_ENV === "PRODUCTION",
+            sameSite:process.env.NODE_ENV==="PRODUCION"?true:false,
             maxAge:7*24*60*60*1000
         })
         return res.json({success:true})
